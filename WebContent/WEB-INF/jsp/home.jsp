a8695fd164375a903e72df45427c662942665d8c
s_furuta <s_furuta@Kas-se-pc28.mshome.net>
2020-11-19 13:24:21 +0900

    基本的なサーブレットの登録

---
+++ WebContent/WEB-INF/jsp/home.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<% out.println("Success!!"); %>
+</body>
+</html>
---
+++ src/jp/ac/o_hara/site/HomeServlet.java
@@ -0,0 +1,30 @@
+package jp.ac.o_hara.site;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+
+		req.getRequestDispatcher("WEB-INF/jsp/home.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doGet(req, resp);
+	}
+}
