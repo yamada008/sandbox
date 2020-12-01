d1a89fb94630759c578403a0c5df663d2e14897d
s_furuta <s_furuta@host.docker.internal>
2020-12-02 08:30:58 +0900

    静的ファイルをバイパスできるようHomeServletを処理

--- src/jp/ac/o_hara/site/HomeServlet.java
+++ src/jp/ac/o_hara/site/HomeServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HomeServlet
  */
-@WebServlet("/")
+@WebServlet("/home")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
